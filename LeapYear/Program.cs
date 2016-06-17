798047b69cc03879c3bfcce2f5351d1ef09011b0
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-17 19:49:39 +1000

    New System
    Higher level code using a method IsLeapYear

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -7,28 +7,31 @@ using System.Threading.Tasks;
 namespace LeapYear {
     class Program {
         static void Main(string[] args) {
+
             bool quit = false;
             while (quit != true) {
-                string entry;
                 Console.WriteLine("Enter year");
-                entry = Console.ReadLine();
+                string entry = Console.ReadLine();
                 int year = Int32.Parse(entry);
-
-                if (year % 4 == 0) {
-                    Console.WriteLine("Yes. {0} is a Leap year", year);
-
-                } else if (year % 100 == 0 && year % 400 == 0) {
-                    Console.WriteLine("Yes. {0} is a Leap year", year);
-
-                } else {
-                    Console.WriteLine("No. {0} is not Leap year", year);
-                }
-
+                IsLeapYear(year);
                 Console.WriteLine("Another year? \nEnter Yes or No.");
                 string another = Console.ReadLine();
                 if (another == "No") {
                     quit = true;
                 }
+            }
+
+        }
+        public static bool IsLeapYear(int year) {
+            if (year % 4 == 0) {
+                Console.WriteLine("Yes. {0} is a Leap year", year);
+                return true;
+            } else if (year % 100 == 0 && year % 400 == 0) {
+                Console.WriteLine("Yes. {0} is a Leap year", year);
+                return true;
+            } else {
+                Console.WriteLine("No. {0} is not Leap year", year);
+                return true;
             }
         }
     }
